e38f5d4dd9d3f50775c321e9691a9a558f101029
Nduduzo Khanyile <nduduzo1@live.com>
2017-11-28 23:14:21 +0200

    MakeABuck and Email Send in Contact Us< Done >

--- web/controllers/homeController.js
+++ web/controllers/homeController.js
@@ -1,3 +1,29 @@
 ﻿app.controller('homeController', function ($http, $scope, $route, $window) {  
+    $scope.price = 3500;
 
+    //Send Mail
+    $scope.SendMail = function(){
+        var name = $scope.name;
+        var number = $scope.number;
+        var message = $scope.message;
+        var email = $scope.email;
+        var subject = $scope.subject;
+
+        var emailObj = {
+            email: "queries@ndu-systems.net",//our email here
+            from: email,
+            name: name,
+            number: number,
+            subject: "Website Query Subject :" + subject,
+            message : message
+        };
+        $http.post("http://ndu-systems.net/Api/emailbeta.php", emailObj)
+                    .success(function (response) {
+                        console.log(response);
+                        alert("Email Sent Successfully!");
+                    })
+                    .error(function (error) {
+                        console.error(error);
+                    });
+    }
 });
